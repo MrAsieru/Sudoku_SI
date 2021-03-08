44fae04b943bfec1005ca3e2fc00372d580667b2
MrAsieru <asiescp@gmail.com>
2021-03-08 19:43:48 +0100

    1.Sprint klaseak sortu

---
+++ Sudoku_SI/src/Modeloa/Gelaxka.java
@@ -0,0 +1,22 @@
+package Modeloa;
+
+import java.util.List;
+
+public class Gelaxka {
+	private int zutabe;
+	private int errenkada;
+	private int zenbakia;
+	private List<Integer> zerAukerak;
+	
+	public Gelaxka(int pEr, int pZu) {
+		//TODO
+	}
+	
+	public void aukerakGehitu(List<Integer> aukerak) {
+		//TODO
+	}
+	
+	public void setZenbakia(int pZenbakia) {
+		//TODO
+	}
+}
---
+++ Sudoku_SI/src/Modeloa/Sudoku.java
@@ -0,0 +1,31 @@
+package Modeloa;
+
+public class Sudoku {
+	private Gelaxka[][] zerGelaxkak;
+	private int[][] soluzioaIntegers;
+	private int zailtasuna;
+	private static Sudoku nireSudoku;
+	
+	private Sudoku(int pZailtasuna) {
+		//TODO
+	}
+	
+	public static Sudoku getNireSudoku(int pZailtasuna) {
+		//TODO
+		return null;
+	}
+	
+	public static Sudoku getNireSudoku() {
+		//TODO
+		return null;
+	}
+	
+	private void sudokuSortu() {
+		//TODO
+	}
+	
+	public boolean ondoDago() {
+		//TODO
+		return false;
+	}
+}
