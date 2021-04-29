ca2c2c83146906eb2511e654392bac159e0b7c34
MrAsieru <asiescp@gmail.com>
2021-04-29 19:03:09 +0200

    3.Sprint-aren egiturak

---
+++ src/Egitura/LaguntzaEgitura.java
@@ -0,0 +1,17 @@
+package Egitura;
+
+public class LaguntzaEgitura {
+	public int errenkada;
+	public int zutabea;
+	public int balioa;
+	public String metodoa;
+
+	public LaguntzaEgitura() {}
+
+	public LaguntzaEgitura(int errenkada, int zutabea, int balioa, String metodoa) {
+		this.errenkada = errenkada;
+		this.zutabea = zutabea;
+		this.balioa = balioa;
+		this.metodoa = metodoa;
+	}
+}
---
+++ src/Egitura/PuntuazioaEgitura.java
@@ -0,0 +1,15 @@
+package Egitura;
+
+public class PuntuazioaEgitura {
+	public String izena;
+	public int zailtasuna;
+	public double puntuazioa;
+
+	public PuntuazioaEgitura() {}
+
+	public PuntuazioaEgitura(String izena, int zailtasuna, double puntuazioa) {
+		this.izena = izena;
+		this.zailtasuna = zailtasuna;
+		this.puntuazioa = puntuazioa;
+	}
+}
