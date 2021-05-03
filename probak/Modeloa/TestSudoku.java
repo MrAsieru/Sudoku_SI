5befb90714df42c0424e621e958408449398f4ca
ikermtz <49227870+ikermtz@users.noreply.github.com>
2021-05-03 20:00:18 +0200

    por si acaso

--- probak/Modeloa/TestSudoku.java
+++ probak/Modeloa/TestSudoku.java
@@ -16,25 +16,21 @@ public class TestSudoku extends TestCase implements Observer{
 		
 		Partida partida;
 		Partida.getPartida();
+		int zail=1;
 		
-		Irakurlea irakurlea= new Irakurlea();
+		int [][] hasi=new int[][] {{9,4,0,3,0,1,7,8,5},{0,8,5,6,4,7,1,0,9},{1,0,2,9,5,8,4,6,0},
+			{5,1,0,4,7,2,3,0,6},{6,9,4,1,8,0,0,7,2},{2,0,7,5,6,9,8,1,0},
+			{0,0,9,8,1,6,2,3,7},{7,6,1,2,3,4,9,0,0},{8,0,3,7,9,5,6,0,1}};
 		
-		
-		SudokuaGorde sg= new SudokuaGorde(1,irakurlea.getSudokuArrayHasiera(0),irakurlea.getSudokuArrayZuzena(0));
-		
-		Sudoku sud= new Sudoku(sg.getSoluzioa(),this);
-		
-		GelaxkaEditagarria gel = new GelaxkaEditagarria(1,2,3);
-		
-		int emaitza = 4;
-		int notEspero = 10;
+		int [][] fin = new int[][] {{9,4,6,3,2,1,7,8,5},{3,8,5,6,4,7,1,2,9},{1,7,2,9,5,8,4,6,3},
+			{5,1,8,4,7,2,3,9,6},{6,9,4,1,8,3,5,7,2},{2,3,7,5,6,9,8,1,4},
+			{4,5,9,8,1,6,2,3,7},{7,6,1,2,3,4,9,5,8},{8,2,3,7,9,5,6,4,1}};
 		
 		
+		SudokuaGorde sg= new SudokuaGorde(zail,hasi,fin);
 		
-		assertEquals(4,gel.getBalioa());
-		
-		assertNotEquals(1,emaitza);
-
+		Partida.getPartida().setZailtasuna(zail);
+		Partida.getPartida().sudokuBerria(this);
 	}
 
 	protected void tearDown() throws Exception {
@@ -46,7 +42,17 @@ public class TestSudoku extends TestCase implements Observer{
 	}
 
 	public void testAldatuGelaxkaBalioa() {
-		fail();
+		
+		int[][] finT = new int[][] {{9,4,6,3,2,1,7,8,5},{3,8,5,6,4,7,1,2,9},{1,7,2,9,5,8,4,6,3},
+			{5,1,8,4,7,2,3,9,6},{6,9,4,1,8,3,5,7,2},{2,3,7,5,6,9,8,1,4},
+			{4,5,9,8,1,6,2,3,7},{7,6,1,2,3,4,9,5,8},{8,2,3,7,9,5,6,4,1}};
+		int[][] hasiT = new int[][] {{9,4,0,3,0,1,7,8,5},{0,8,5,6,4,7,1,0,9},{1,0,2,9,5,8,4,6,0},
+			{5,1,0,4,7,2,3,0,6},{6,9,4,1,8,0,0,7,2},{2,0,7,5,6,9,8,1,0},
+			{0,0,9,8,1,6,2,3,7},{7,6,1,2,3,4,9,0,0},{8,0,3,7,9,5,6,0,1}};
+		
+		Sudoku sud;
+		
+		assertEquals(hasiT, finT);
 	}
 
 	public void testGelaxkaHautagaiaLortu() {
