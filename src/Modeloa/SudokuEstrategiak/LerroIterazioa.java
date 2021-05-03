6c1e8e4d724d025d355b9dcfcd6d4a0f1ac157ba
EnekoP <49227962+EnekoP@users.noreply.github.com>
2021-05-03 20:35:26 +0200

    Ronaldinho gausho

--- src/Modeloa/SudokuEstrategiak/LerroIterazioa.java
+++ src/Modeloa/SudokuEstrategiak/LerroIterazioa.java
@@ -1,9 +1,23 @@
 package Modeloa.SudokuEstrategiak;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class LerroIterazioa implements LaguntzaMetodoa {
 	public Integer laguntza(int pErr, int pZut) {
-		//TODO
-		return null;
+		int laguntza=0;
+		int rand=0;
+		Random random= new Random();
+		rand=random.nextInt(10);
+		rand+=1;
+		ArrayList<Integer> aukerak= new ArrayList<Integer>();
+		aukerak.add(rand);
+		
+		
+		if (aukerak.size()==1){
+			laguntza=aukerak.get(0);
+		}
+		return laguntza;
 	}
 
 	public String getMetodoIzena(){
--- src/Modeloa/SudokuEstrategiak/SoleCandidate.java
+++ src/Modeloa/SudokuEstrategiak/SoleCandidate.java
@@ -1,9 +1,29 @@
 package Modeloa.SudokuEstrategiak;
+import java.util.ArrayList;
+
+import Modeloa.Partida;
+import Modeloa.Gelaxka.Gelaxka;
+import Modeloa.Sudokua.*;
 
 public class SoleCandidate implements LaguntzaMetodoa {
 	public Integer laguntza(int pErr, int pZut) {
-		//TODO
-		return null;
+		Integer laguntza=null;
+		boolean [] help1= new boolean[9];
+		boolean [] help2= new boolean[9];
+		boolean [] help3= new boolean[9];
+		ArrayList <Integer> aukerak = new ArrayList<Integer>();
+		help1 = Partida.getPartida().getSudoku().errenkadaHautagaiak(pErr);
+	    help2 = Partida.getPartida().getSudoku().zutabeHautagaiak(pZut);
+		help3 = Partida.getPartida().getSudoku().eremuHautagaiak(pErr, pZut);
+		for(int i=0; i<9 ;i++){
+			if(help1[i] && help2[i] && help3[i]){
+				aukerak.add(i+1);
+			}	
+		}
+		if (aukerak.size()==1){
+			laguntza=aukerak.get(0);
+		}
+		return laguntza;
 	}
 
 	public String getMetodoIzena(){
